88f1ef450d94c6fb223a49063f77a2098ac56f5d
noracamacho <ingnorac@gmail.com>
2023-02-22 05:16:06 -0600

    Adding style to MovieList container

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -12,7 +12,7 @@ export const MovieList = ({ apiPath, title }) => {
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-        <div className="flex justify-start flex-wrap other:justify-evenly">
+        <div className="flex justify-evenly flex-wrap">
           { movies?.map((movie) => (
             <MovieCard key={movie.id} movie={movie}/>
           ))
